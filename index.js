49d210f87909c8fbe644a621030c85bf776fbf5e
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 22:14:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  kittens = kittens.slice(0, kittens.length - 1)
+  var kittens = kittens.slice(0, kittens.length - 1)
   return kittens
 }
